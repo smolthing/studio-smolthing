404a7458afb95b68900daf7f140d1e24e5b3451b
mysmolthing <mysmolthing@gmail.com>
2024-11-17 19:25:31 +0800

    Email can't be forwarded, change text to be less confusing

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Page() {
             <div className="relative">
               <input
                 className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-25 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                placeholder="Enter email to join our newsletter"
+                placeholder="Join our newsletter"
                 onKeyDown={handleEnterKey}
               />
               <button
