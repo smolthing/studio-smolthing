af0ca409d843324d6c749889a4efa5e83c0761e9
mysmolthing <mysmolthing@gmail.com>
2024-11-26 22:34:43 +0800

    Google analytics, not GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 import GoogleFontKoh from "./common/font/GoogleFontKoh";
-import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 let title = "Studio Smolthing";
 let description = "I make tech cute";
@@ -35,7 +35,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="G-G9Y7DDDB6R" />
+      <GoogleAnalytics gaId="G-G9Y7DDDB6R" />
       <body className={GoogleFontKoh.variable}>{children}</body>
     </html>
   );
